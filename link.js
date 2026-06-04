6fc42648f513b9c47cc3c7ca9cd392b84aae0732
SayloIs1 <whyissaylo@gmail.com>
2026-06-03 20:56:32 -0700

    Server Boot: Remote API update to live proxy link

--- link.js
+++ link.js
@@ -1,2 +1,2 @@
 // Auto-generated live configuration
-const LIVE_SERVER_URL = "wss://7642080260e8e063-76-158-48-134.serveousercontent.com";
+const LIVE_SERVER_URL = "wss://5da0b1be0bc7191f-76-158-48-134.serveousercontent.com";
